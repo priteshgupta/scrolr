1cf81804a1af9964a1b11aa8be3da737ab8817f2
Gabriel Holodak <gthepiper@gmail.com>
2013-10-19 12:26:54 -0400

    adding some plugin support

--- extension/myscript.js
+++ extension/myscript.js
@@ -1,9 +1,28 @@
+/*
 console.log("window.pageYOffset " + window.pageYOffset);
 console.log("document.documentElement.scrollTop " + document.documentElement.scrollTop);
 console.log("document.body.scrollTop " + document.body.scrollTop);
 var top = document.body.scrollTop;
-var n = 1;
-setInterval(function(){
-  window.scrollBy(0, 1);
-}, 20)
+var n = 1; */
+alert("asdf");
 
+function createEmbed(width,height){
+  var embed = document.createElement('embed');
+  embed.setAttribute('id', "plugin0")
+  embed.setAttribute('width',width);
+  embed.setAttribute('height',height);
+  embed.setAttribute('type',"application/x-scrolrplugin");
+  //var div = document.getElementById(replaceid);
+  //document.getElementsByTagName('body')[0].replaceChild(embed,div); 
+  document.body.appendChild(embed);
+}
+
+createEmbed('0','0');
+var plugin = document.getElementById("plugin0");
+
+alert(plugin.version);
+alert(plugin.getCurrentHeadDirection());
+
+setInterval(function(){
+  window.scrollBy(0, plugin.getCurrentHeadDirection());
+}, 50);
