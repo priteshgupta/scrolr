b2057e85807ef21ce1cabb1a3d2a7e84da5d6032
Gabriel Holodak <gthepiper@gmail.com>
2013-10-19 22:25:53 -0400

    functional background page

--- extension/background.js
+++ extension/background.js
@@ -1,5 +1,19 @@
+/*
 // React when a browser action's icon is clicked.
 chrome.browserAction.onClicked.addListener(function(tab) {
   //myscript.js is injected into the current page
   chrome.tabs.executeScript(null, {file: "myscript.js"}); 
 });
+*/
+
+function plugin0()
+{
+	return document.getElementById('plugin0');
+}
+window.onload = function () {
+  var plugin = plugin0();
+  document.write("version = " + plugin.version);
+  document.write("tracking = " + plugin.tracking);
+  alert("hi!");
+};
+
