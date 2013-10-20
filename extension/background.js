c2638392c27217f7e4cdab0fe12786ac28d7ad4f
Gabriel Holodak <gthepiper@gmail.com>
2013-10-20 00:11:20 -0400

    working on background functionality for extension

--- extension/background.js
+++ extension/background.js
@@ -5,15 +5,25 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript(null, {file: "myscript.js"}); 
 });
 */
-
-function plugin0()
-{
-	return document.getElementById('plugin0');
+function plugin0 (){
+  return document.getElementById('plugin0');
 }
+
+function appendToDoc(text){
+  console.log("adding text: " + text);
+  var placeholder = document.getElementById('placeholder');
+  placeholder.appendChild(document.createTextNode(text));
+  placeholder.appendChild(document.createElement('br'));
+}
+
 window.onload = function () {
-  var plugin = plugin0();
-  document.write("version = " + plugin.version);
-  document.write("tracking = " + plugin.tracking);
+  //var plugin = plugin0();
   alert("hi!");
+  //plugin.tracking = true;
+  //plugin.initialize();
+  //alert("initializing");
+  //appendToDoc("version = " + plugin.version);
+  //appendToDoc("tracking = " + plugin.tracking);
+  //alert("should now be tracking: " + plugin.tracking);
 };
 
