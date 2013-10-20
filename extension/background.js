403a9201a4b9a766fab52f652f028b94ac669936
Gabriel Holodak <gthepiper@gmail.com>
2013-10-20 11:43:34 -0400

    added x-scrolling, reinitialization in extension

--- extension/background.js
+++ extension/background.js
@@ -1,47 +1,38 @@
 var tracking = false;
 
-
 function plugin0 (){
-    return document.getElementById('plugin0');
-}
-
-function appendToDoc(text){
-    console.log("adding text: " + text);
-    var placeholder = document.getElementById('placeholder');
-    placeholder.appendChild(document.createTextNode(text));
-    placeholder.appendChild(document.createElement('br'));
+  return document.getElementById('plugin0');
 }
 
 function runTrackingLoop(){
-    var plugin = plugin0();
-    setInterval(function(){  
-        if(tracking){
-            var ret = plugin.track();
-            console.log(ret);
-            chrome.tabs.executeScript(null, {code: "window.scrollBy(0, "+ret+");"});
-        }
-    },50);
+  var plugin = plugin0();
+  setInterval(function(){  
+    if(tracking){
+      var ret = plugin.track().split(" ", 2);
+      console.log(ret);
+      chrome.tabs.executeScript(null, 
+        {code: "window.scrollBy(" + ret[0] + ", " + ret[1] + ");"});
+    }
+  },50);
 }
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-    if(tracking){
-        tracking = false;
-        chrome.browserAction.setIcon({path: "icon-32-grey.png"});
-    } else {
-        tracking = true;
-        chrome.browserAction.setIcon({path: "icon-32.png"});
-        //runTrackingLoop();
-    }
+  // toggle tracking and button icon
+  if(tracking){
+    tracking = false;
+    chrome.browserAction.setIcon({path: "icon-32-grey.png"});
+  } else {
+    plugin0().initialize();
+    chrome.browserAction.setIcon({path: "icon-32.png"});
+    tracking = true;
+  }
 });
 
 window.onload = function () {
-    var plugin = plugin0();
-    //alert("ready?");
-    console.log("initializing");
-    plugin.initialize();
-    tracking = true;
-    console.log("starting tracking loop");
-    runTrackingLoop();
-    //appendToDoc("version = " + plugin.version);
-    //appendToDoc("tracking = " + plugin.tracking);
+  var plugin = plugin0();
+  console.log("initializing");
+  plugin.initialize();
+  tracking = true;
+  console.log("starting tracking loop");
+  runTrackingLoop();
 };
